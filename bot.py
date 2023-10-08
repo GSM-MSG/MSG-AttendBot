bfd74eac8587052d2fb2db955d7c166dab6f232d
shk0625 <tjfgml1238@gmail.com>
2023-10-08 14:00:28 +0900

    Update :: bot.py 중간 save

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import asyncio
+import logging
 import discord
+from discord import client
+from discord.ext import commands
+from attendance import Member, table_init, get_all_attendance_info, scoreboard
+
+from config import timezone
+
+logger = logging.getLogger(__name__)
+lock = asyncio.Lock()
+
+
+async def process_commands(self, message):
+    ctx = await self.get_contect(message)
+    await self.invoke(ctx)
+
+
+commands.bot.BotBase.process_commands = process_commands
+
+intents = discord.Intents(messages=True, guilds=True, members=Ture)
+
