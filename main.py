4ec9023a2c1cb8900085843aab7d675fe51c540d
shk0625 <tjfgml1238@gmail.com>
2023-10-16 14:24:52 +0900

    ⚡️ Update :: token env로 관리

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
 
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+load_dotenv()
+
+token=os.getenv('AttendBot_TOKEN')
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-AttendBot_TOKEN
+bot.run(token)
+
+
