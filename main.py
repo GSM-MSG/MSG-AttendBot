67b4bbf0e8dee9c5acf1cd8ea514bd509ccbde67
shk0625 <tjfgml1238@gmail.com>
2023-10-16 19:58:03 +0900

    ✨ Update :: n분 뒤 개인 DM으로 재촉기능

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -19,10 +21,21 @@ async def testHello(ctx):
     await ctx.channel.send(f'{ctx.message.author.mention}님, 나도 안녕!', reference=ctx.message)
 
 
-@bot.command(name="독촉") # 매일 아침 8시 (주말제외?) 개인 DM으로 쓰라고 연락옴
+@bot.command(name="독촉")  # 매일 아침 8시 (주말제외?) 개인 DM으로 쓰라고 연락옴
 async def follow(ctx):
     dm_channel = await ctx.message.author.create_dm()
-    await dm_channel.send(f'{ctx.message.author.mention}님, 데일리 적고 공유해주세요!')
+    await dm_channel.send(f'{ctx.message.author.mention}님, 출석하세요! 데일리 적고 공유해주세요!')
+
+
+@bot.command(name="알람")
+async def alarm(ctx, duration: int):
+    if duration not in [3, 5, 7]:
+        await ctx.send("3, 5, 7분 뒤 재알람만 가능합니다.")
+        return
+
+    await ctx.send(f"{duration}분 후에 재알람 설정이 되었습니다. **출석**과 **데일리**를 성실하게 해주세요 오늘도 파이팅 ٩( ᐛ )و")
+    await asyncio.sleep(duration)
+    await ctx.author.send(f"{duration}분이 지났습니다. `/출석`, `/데일리작성` 명령어를 사용하세요.")
 
 
 @bot.command(name="도움말")
