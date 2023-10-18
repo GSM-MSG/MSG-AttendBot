1a0c9d03b93a7513a87b707a013e5fdc4e87c190
shk0625 <tjfgml1238@gmail.com>
2023-10-18 12:17:59 +0900

    Revert "Revert "🎨 Update :: DB Class 나누기""
    This reverts commit 00e491f2d937c775ead5ee314bc63cb7b5693e49.

---
+++ connection.py
@@ -0,0 +1,27 @@
+import os
+import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Connection:
+    def __init__(self):
+        self.host = os.getenv('DB_HOST')
+        self.user = os.getenv('DB_USER')
+        self.pw = os.getenv('DB_PASSWORD')
+        self.db = os.getenv('DB_SCHEMA')
+        self.conn = pymysql.connect(host=self.host, user=self.user, password=self.pw, database=self.db)
+        self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
+        print("DB에 성공적으로 연결됨")
+
+    def close_connection(self):
+        self.conn.close()
+        print("DB 연결을 끊음")
+
+    def get_connection(self):
+        self.conn.ping()
+        return self.conn, self.cur
+
+
+connection = Connection()
