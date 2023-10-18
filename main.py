78163d39c84c3650142aa5fd1a7faf1aba755cb3
shk0625 <tjfgml1238@gmail.com>
2023-10-18 19:50:39 +0900

    🎨 Update :: 함수 호출로 변경

--- main.py
+++ main.py
@@ -15,28 +15,8 @@ load_dotenv()
 token = os.getenv('AttendBot_TOKEN')
 channel_id = os.getenv('CHANNEL_ID')
 
-
-class Connection:
-    def __init__(self):
-        self.host = os.getenv('DB_HOST')
-        self.user = os.getenv('DB_USER')
-        self.pw = os.getenv('DB_PASSWORD')
-        self.db = os.getenv('DB_SCHEMA')
-        self.conn = pymysql.connect(host=self.host, user=self.user, password=self.pw, database=self.db)
-        self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
-        print("DB에 성공적으로 연결됨")
-
-    def __del__(self):
-        self.conn.close()
-        print("DB 연결을 끊음")
-
-    def getConnection(self):
-        self.conn.ping()
-        return self.conn, self.cur
-
-
-connection = Connection()
-c = connection.cur
+connection = connection.Connection()
+conn, cur = connection.getConnection()
 
 
 @bot.event
